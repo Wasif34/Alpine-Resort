8e30014eb21ed531e30522a5935b23b4b585bc3d
Wasif34 <wasif.shahid@acruxtek.com>
2025-02-17 22:39:10 +0500

    security policies

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://awazfccevasvubsusjpd.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3YXpmY2NldmFzdnVic3VzanBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3NTg3NzIsImV4cCI6MjA1NDMzNDc3Mn0.jLBxx34MvN17EE9QzhOk7ZOxIFxgB6qaf-ipv7DL4rc";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
